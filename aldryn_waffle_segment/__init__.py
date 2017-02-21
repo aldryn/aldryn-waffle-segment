1560f13c6b24c8faa76e494e5ab3df9d8b1f9f92
Stefan Foulis <stefan@foulis.ch>
2017-02-21 15:49:01 +0100

    Release 0.0.2

--- aldryn_waffle_segment/__init__.py
+++ aldryn_waffle_segment/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 default_app_config = 'aldryn_waffle_segment.apps.AldrynWaffleSegmentAppConfig'
